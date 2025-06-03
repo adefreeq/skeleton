b3d8adf657f093e104598b124fcc611e3057c2a8
adefreeq <ade@frequencyadvisors.com>
2025-06-03 02:19:06 +0100

    Clean up code

--- ui/src/main/java/context/SpringConfig.java
+++ ui/src/main/java/context/SpringConfig.java
@@ -1,9 +1,15 @@
 package context;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "ui")
 public class SpringConfig {
+
+    @Bean
+    public ScenarioContext scenarioContext() {
+        return new ScenarioContext();
+    }
 }
